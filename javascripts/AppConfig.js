281d5b8e9306cea2219930911ef847e2692f1bb8
Debra Gordon <debra.m.gordon@gmail.com>
2017-04-28 18:52:14 -0500

    built out the bones

---
+++ javascripts/AppConfig.js
@@ -0,0 +1,9 @@
+"use strict";
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'partials/main.html'
+        })
+        .otherwise('/home');
+});
