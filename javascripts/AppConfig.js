88f591e2a11202a6dd626bdb623fe32038b2e11e
Debra Gordon <debra.m.gordon@gmail.com>
2017-05-06 11:24:01 -0500

    updating content

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -2,8 +2,8 @@
 
 app.config(function($routeProvider) {
     $routeProvider
-        .when('/home', {
-            templateUrl: 'partials/main.html'
-        })
+        // .when('/home', {
+        //     templateUrl: 'partials/main.html'
+        // })
         .otherwise('/home');
 });
