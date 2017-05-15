7098960d79c0b4c8f58922d3061d0bd0f0fcd2bb
Debra Gordon <debra.m.gordon@gmail.com>
2017-05-15 12:45:59 -0500

    added tech logos and fixed bugs after last merge

--- javascripts/Controllers/HomeCtrl.js
+++ javascripts/Controllers/HomeCtrl.js
@@ -9,7 +9,27 @@ app.controller("HomeCtrl", function ($scope, $rootScope, $http){
   }
   );
 
-  $scope.techIcons = ["/images/tech/angular.png", "/images/tech/Bootstrap.png", "/images/tech/Bower.png", "/images/tech/cSharp.png", "/images/tech/css.png", "/images/tech/dotNet.png", "/images/tech/firebase.png", "/images/tech/git.png", "/images/tech/github.png", "/images/tech/html5.png", "/images/tech/javascript.png", "/images/tech/materialize.png", "/images/tech/moqups.png", "/images/tech/npm.png", "/images/tech/sass.png", "/images/tech/SQLite.png", "/images/tech/Trello.png"];
+  $scope.techIcons = [{ img:"/images/tech/angular.png", title: "Angular"},
+                  { img:"/images/tech/Bootstrap.png", title: "Bootstrap"},
+                  { img:"/images/tech/Bower.png", title: "Bower"},
+                  { img:"/images/tech/cSharp.png", title: "C#"},
+                  { img:"/images/tech/css.png", title: "CSS"},
+                  { img:"/images/tech/dotNet.png", title: "Dot Net"},
+                  { img:"/images/tech/firebase.png", title: "Firebase"},
+                  { img:"/images/tech/git.png", title: "Git"},
+                  { img:"/images/tech/github.png", title: "Github"},
+                  { img:"/images/tech/grunt.png", title: "Grunt"},
+                  { img:"/images/tech/html5.png", title: "HTML5"},
+                  { img:"/images/tech/javascript.png", title: "Javascript"},
+                  { img:"/images/tech/jquery.png", title: "jQuery"},
+                  { img:"/images/tech/materialize.png", title: "Materialize"},
+                  { img:"/images/tech/moqups.png", title: "MOQ"},
+                  { img:"/images/tech/npm.png", title: "NPM"},
+                  { img:"/images/tech/sass.png", title: "SASS"},
+                  { img:"/images/tech/sql.png", title: "SQL"},
+                  { img:"/images/tech/structuremap.png", title: "Structure Map"},
+                  { img:"/images/tech/Trello.png", title: "Trello"},
+                  ];
 
   $scope.clear = function () {
     document.getElementById("search").value = "";
@@ -17,6 +37,5 @@ app.controller("HomeCtrl", function ($scope, $rootScope, $http){
   };
 
 });
-
 
 
